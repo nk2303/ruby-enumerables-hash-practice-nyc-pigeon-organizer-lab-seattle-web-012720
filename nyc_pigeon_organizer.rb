1ad1496f00f8b5f21e78d0f334d0ff01c6ee7df0
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-14 09:00:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,15 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  name = []
-  
-  data[:color]
+  list = []
+  data[:color].each{ |name| list.push(name) }
+  pp list
+  name_list = []
+  list.length.times do |block|
+    puts list[block][1]
+    list[block][1].length.times do |n|
+      name_list.push(list[block][1][n])
+    end
+  end
+  pp name_list.uniq
   return pigeon_list
 end
